b851548be35a95462fc4f00a7512218710762e2f
Ajay-stackdesign <ajaygsahani968@gmail.com>
2022-02-02 23:46:23 +0530

    changes

---
+++ blog/api/serializer.py
@@ -0,0 +1,10 @@
+from dataclasses import field
+from pyexpat import model
+from rest_framework import serializers
+from .models import Room
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        field = ('id', 'code', 'host', 'guest_can_pause', 'votes_to_skip', 'created_at')
