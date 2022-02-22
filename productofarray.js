ca42bcf8c6126fe69de34f82984afb849a7c5850
Ajay-stackdesign <ajaygsahani968@gmail.com>
2022-02-22 23:42:45 +0530

    changes

---
+++ productofarray.js
@@ -0,0 +1,12 @@
+// function productOfArray(arr) {
+//     if (arr.length === 0) return 1
+//     console.log(arr)
+//     return arr[0] + productOfArray(arr.slice(1))
+// }
+// arr = [1, 3, 2, 4, 53, 6, 8]
+// result = productOfArray(arr)
+// console.log(result)
+
+var arr = [1, 2, 3]
+result = arr.slice(2)
+console.log(result)
